d4f22ca75b001aca9c94cc0578bc79bdc81e709e
Vladimir <vladimir@alphablues.com>
2022-07-27 16:38:16 +0300

    added, ssr and routers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router,
+  Switch,
+  Route,
+  Link} from "react-router-dom";
+import Routers from'./Routers' 
+
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router className="App">
+      <h1>Server-Side Rendering Example</h1>
+        <ul>
+          <li>
+            <Link to='/' >Home</Link>
+          </li>
+          <li>
+            <Link to='/about' >About</Link>
+          </li>
+          <li>
+            <Link to='/articles' >Articles</Link>
+          </li>
+        </ul>
+       <Routers />
+    </Router>
   );
 }
 
